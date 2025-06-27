6c619dea665846fac73894eeed47b51702bfbb1a
CosmoSouza <lucascap9@gmail.com>
2025-06-27 15:13:31 -0300

    Adicionando arquivos Java da pasta src

--- src/application/Program.java
+++ src/application/Program.java
@@ -14,7 +14,7 @@ public class Program {
 		Scanner sc = new Scanner(System.in);
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 		
-		System.out.println("Room number: ");
+		System.out.print("Room number: ");
 		int number = sc.nextInt();
 		System.out.print("Check-in date (dd/MM/yyyy): ");
 		Date checkIn = sdf.parse(sc.next());
@@ -35,15 +35,11 @@ public class Program {
 			System.out.print("Check-out date (dd/MM/yyyy): ");
 			checkOut = sdf.parse(sc.next());
 			
-			Date now = new Date();
-			if (checkIn.before(now) || checkOut.before(now)){
-				System.out.println("Error in reservation: Reservation dates for updates must be future dates");
-			}
-			else if (!checkOut.after(checkIn)) {
-				System.out.println("Error in reservation: Check-out date must be after check-in date");
+			String error = reservation.updatesDates(checkIn, checkOut);
+			if (error != null) {
+				System.out.println("Error in reservation: " + error);
 			}
 			else {
-			reservation.updatesDates(checkIn, checkOut);
 			System.out.println("Reservation: " + reservation);
 		}
 	}
